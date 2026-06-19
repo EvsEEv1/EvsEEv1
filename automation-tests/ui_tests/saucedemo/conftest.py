9a79ffac713b3ddee67bc82928b1f2c61057d4b6
Evseev_Evsey <powerfulpugilist@gmail.com>
2026-06-19 17:07:32 +0700

    Чиню Selenium, выключаю всплывающее окно в браузере

--- automation-tests/ui_tests/saucedemo/conftest.py
+++ automation-tests/ui_tests/saucedemo/conftest.py
@@ -7,10 +7,23 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")         
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-save-password-bubble")
+    chrome_options.add_argument("--disable-password-manager-reauthentication")
+    chrome_options.add_experimental_option("prefs", {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.default_content_setting_values.notifications": 2
+    })
+
+    browser = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
