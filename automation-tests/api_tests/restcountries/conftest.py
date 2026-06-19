5b64d2583eb33cd82cef5193b0b221884c2fedff
Evseev_Evsey <powerfulpugilist@gmail.com>
2026-06-19 16:57:48 +0700

    Изменил название файла с тестами и исправил зависимости в CI

--- automation-tests/api_tests/restcountries/conftest.py
+++ automation-tests/api_tests/restcountries/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 import requests
+import os 
 
-API_KEY = "rc_live_ce2351459f984e228024a777fe78e85a"
+API_KEY = os.getenv("API_KEY", "rc_live_ce2351459f984e228024a777fe78e85a")
 
 @pytest.fixture(scope="session")
 def api():
