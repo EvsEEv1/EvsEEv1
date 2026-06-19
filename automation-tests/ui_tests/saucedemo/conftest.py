8daf4d6f44ba6739c25d3dd3d7b0b8768505ad4c
Evseev_Evsey <powerfulpugilist@gmail.com>
2026-06-19 16:48:34 +0700

    Добавил установщик Chrome и безоконный режим

--- automation-tests/ui_tests/saucedemo/conftest.py
+++ automation-tests/ui_tests/saucedemo/conftest.py
@@ -2,13 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    browser = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install())
-    )
-    browser.implicitly_wait(5)  
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    browser.implicitly_wait(5)
     yield browser
     browser.quit()
